088d48bcbb9463edb6bfa8a3f5b9951b6594def1
Ingles200h <alexandre.rwada@gmail.com>
2018-08-28 12:23:13 -0300

    Perfil para todos

--- application/models/Perfil_Model.php
+++ application/models/Perfil_Model.php
@@ -4,7 +4,7 @@
 class Perfil_Model extends CI_Model{
 
 	public function getAll() {
-		$query = $this->db->query("SELECT * FROM appleplanet.tb_perfil WHERE tb_perfil.id_loja = ?",array($_SESSION['id_loja']));
+		$query = $this->db->query("SELECT * FROM appleplanet.tb_perfil");
 
 		if($query->num_rows() > 0 ) {
 
